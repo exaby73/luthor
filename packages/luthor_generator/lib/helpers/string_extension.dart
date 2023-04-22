587c2ee85effc7a604bb2c2de9d83069df40a2c6
Nabeel Parkar <nabeelparkar99@gmail.com>
2023-04-22 08:17:22 +0530

    Generate private schema and require a schema static variable to be defined on the class

--- packages/luthor_generator/lib/helpers/string_extension.dart
+++
@@ -1,7 +0,0 @@
-extension StringX on String {
-  String pascalToCamelCase() {
-    if (isEmpty) return this;
-    if (length == 1) return toLowerCase();
-    return '${this[0].toLowerCase()}${substring(1)}';
-  }
-}

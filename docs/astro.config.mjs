f64d9f2ab93fce55ffd35aac0c4e753cdebba150
exaby73 <nabeelparkar99@gmail.com>
2024-06-14 11:49:35 +0530

    feat: change site to https://luthor.netlify.app

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://luthor.dev",
+  site: "https://luthor.netlify.app",
   base: "/",
   integrations: [
     starlight({

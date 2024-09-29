acb3b6f91eb8dc64e95eebf4f1241a206da84893
exaby73 <nabeelparkar99@gmail.com>
2024-09-30 02:50:17 +0530

    test: Fix tests and add new tests

--- packages/luthor/test/validations/strings/string_ip_validation_test.dart
+++ packages/luthor/test/validations/strings/string_ip_validation_test.dart
@@ -2,7 +2,7 @@ import 'package:luthor/luthor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('should return true when value is a valid ip', () {
+  test('should return true when value is a valid IP', () {
     final result = l
         .string()
         .ip()
@@ -16,18 +16,44 @@ void main() {
     }
   });
 
-  test('should return false when value is not a valid ip', () {
+  test('should return false when value is not a valid IP', () {
     final result = l.string().ip().validateValue('256.1.1.1');
 
     switch (result) {
       case SingleValidationSuccess(data: _):
         fail('should not be a success');
       case SingleValidationError(data: _, errors: final errors):
-        expect(errors, ['value must be a valid ip address']);
+        expect(errors, ['value must be a valid IP address']);
     }
   });
 
-  test('should return true when value is a valid ipv6 ', () {
+  test('should return true when value is a valid IPv4 ', () {
+    final result =
+        l.string().ip(version: IpVersion.v4).validateValue('192.168.1.1');
+
+    switch (result) {
+      case SingleValidationSuccess(data: _):
+        expect(result.data, '192.168.1.1');
+      case SingleValidationError(data: _, errors: _):
+        fail('should not have errors');
+    }
+  });
+
+  test('should return false when the value is not a valid IPv4 address', () {
+    final result = l
+        .string()
+        .ip(version: IpVersion.v4)
+        .validateValue('84d5:51a0:9114:1855:4cfa:f2d7:1f12:7003');
+
+    switch (result) {
+      case SingleValidationSuccess(data: _):
+        fail('should not be a success');
+      case SingleValidationError(data: _, errors: final errors):
+        expect(errors, ['value must be a valid IPv4 address']);
+    }
+  });
+
+  test('should return true when value is a valid IPv6 ', () {
     final result = l
         .string()
         .ip(version: IpVersion.v6)
@@ -41,15 +67,15 @@ void main() {
     }
   });
 
-  test('should return true when value is a valid ipv4 ', () {
+  test('should return false when the value is not a valid IPv6 address', () {
     final result =
-        l.string().ip(version: IpVersion.v4).validateValue('192.168.1.1');
+        l.string().ip(version: IpVersion.v6).validateValue('192.168.1.1');
 
     switch (result) {
       case SingleValidationSuccess(data: _):
-        expect(result.data, '192.168.1.1');
-      case SingleValidationError(data: _, errors: _):
-        fail('should not have errors');
+        fail('should not be a success');
+      case SingleValidationError(data: _, errors: final errors):
+        expect(errors, ['value must be a valid IPv6 address']);
     }
   });
 
